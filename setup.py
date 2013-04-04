bfa9d57a3ffa3f08e5758d757cc887d0897dd64b
Andre Anjos <andre.anjos@idiap.ch>
2013-04-04 13:35:44 +0200

    Final 1.1.x branch release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='antispoofing.motion',
-    version='1.1.0a0',
+    version='1.1.0',
     description='Motion counter-measures for the REPLAY-ATTACK database',
     url='http://pypi.python.org/pypi/antispoofing.motion',
     license='GPLv3',
